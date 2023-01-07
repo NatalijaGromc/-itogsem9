d1709b469537be4b95e7ad36a6e69f5cbd1d69da
natasha <gromc-natalija@rambler.ru>
2023-01-07 17:48:25 +0300

    итоговый проект

---
+++ task_64/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+
+void writeNumbersRecurrecntly(int N){
+    if (N>=1){      
+        Console.Write($"{N} ");
+        writeNumbersRecurrecntly(N-1);
+    }
+};
+
+Console.Write("Вывод N чисел: ");
+writeNumbersRecurrecntly(N);
---
+++ task_66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int N,M;
+int sum = 0;
+
+Console.WriteLine(" Задайте значения M и N. Программа найдёт сумму натуральных элементов в промежутке от M до N.");
+Console.Write("Введите число M: ");
+int.TryParse(Console.ReadLine(), out M);
+
+Console.Write("Введите число N, большее, чем M: ");
+int.TryParse(Console.ReadLine(), out N);
+while (N<=M){
+    Console.Write("Введите число N, большее, чем M: ");
+    int.TryParse(Console.ReadLine(), out N);
+}
+
+
+for (int i=M;i<=N;i++){
+    sum+=i;
+}
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {M} до {N}: {sum}");
